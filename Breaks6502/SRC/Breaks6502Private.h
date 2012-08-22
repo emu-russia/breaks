50ba2eb7f0c13ff09e6d947bb9719e452e40af2c
ogamespec@gmail.com <ogamespec@gmail.com@92c399df-91ff-7c34-b1d0-cc9d408d2d33>
2012-08-22 13:57:53 +0000

    Breaks6502 core subtree

---
+++ Breaks6502/SRC/Breaks6502Private.h
@@ -0,0 +1,20 @@
+
+// Top part components
+
+#include "MISC.h"
+#include "TIME_REG.h"
+#include "IR.h"
+#include "PREDECODE.h"
+#include "PLA.h"
+#include "RANDOM_LOGIC.h"
+
+// Bottrom part components
+
+#include "ADDR_BUS.h"
+#include "DATA_BUS.h"
+#include "REGS.h"
+#include "ALU.h"
+#include "PROGRAM_COUNTER.h"
+
+
+#define BIT (n)     ( (n) & 1 )
---
+++ Breaks6502/SRC/make.bat
@@ -0,0 +1,3 @@
+set PATH=c:\lcc\bin
+
+lc -g2 IR.c TIME_REG.c MISC.c RANDOM_LOGIC.c PREDECODE.c PLA.c REGS.c PROGRAM_COUNTER.c ALU.c ADDR_BUS.c DATA_BUS.c Breaks6502.c TestSuite.c -o Breaks6502.exe
