8b93a8d76985ec16757459114055aab020dff1ec
ogamespec@gmail.com <ogamespec@gmail.com@92c399df-91ff-7c34-b1d0-cc9d408d2d33>
2012-09-04 15:15:38 +0000

    - TIME REG ready

--- Breaks6502/SRC/TestSuite.c
+++ Breaks6502/SRC/TestSuite.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
 #include "Breaks6502.h"
+#include "Breaks6502Private.h"
 
 Context6502 cpu;
+
+// Timing register
+// Imitate begaviour of long CPU instruction.
+void TimeRegTest (void)
+{
+    int r = 20, TR;
+    int sync = 2;
+
+    while (r--)
+    {
+        cpu.PHI1 = BIT(~cpu.PHI0);
+        cpu.PHI2 = BIT(cpu.PHI0);
+        TR = TcountRegister (&cpu, sync > 0, 1, 0);
+        printf ( "%02X ", TR );
+        cpu.PHI0 ^= 1;
+        sync--;
+    }
+
+    // Output should be like: 0F 0F 0E 0E 0D 0D 0B 0B 07 07 0F 0F 0F 0F 0F 0F 0F 0F 0F 0F
+}
 
 // http://visual6502.org/wiki/index.php?title=6502DecimalMode
 void ALUTest (void)
@@ -17,19 +38,22 @@ void GeneralExecutionTest (i)
     }
 }
 
-main ()
+// Execute some clocks to reset 6502
+void Reset (void)
 {
     int r = 100;
-
-    // Initial conditions
-    cpu.PHI0 = 0;
-
-    // Execute some clocks to reset 6502
     while (r--) {
         Step6502 ( &cpu );
         cpu.PHI0 ^= 1;
     }
+}
 
-    ALUTest ();
+main ()
+{
+    // Initial conditions
+    memset (&cpu, 0, sizeof(cpu));
+
+    TimeRegTest ();
+    //ALUTest ();
     //GeneralExecutionTest ( 10000 );
 }
