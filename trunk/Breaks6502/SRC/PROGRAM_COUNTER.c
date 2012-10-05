b54cf5dc1782391cf085ce13196bc76c2f9466b4
ogamespec@gmail.com <ogamespec@gmail.com@92c399df-91ff-7c34-b1d0-cc9d408d2d33>
2012-10-05 10:50:44 +0000

    - program counter started

--- trunk/Breaks6502/SRC/PROGRAM_COUNTER.c
+++ trunk/Breaks6502/SRC/PROGRAM_COUNTER.c
@@ -6,4 +6,41 @@
 
 void ProgramCounter (Context6502 * cpu)
 {
+    int i, PCLC = -1, PCHC, carry_in, carry_out;
+
+    // LOW
+
+    carry_out = cpu->DRIVEREG[DRIVE_IPC];
+
+    for (i=0; i<8; i++) {
+        if (cpu->DRIVEREG[DRIVE_ADL_PCL]) cpu->PCLS[i] = cpu->ADL[i];
+        if (cpu->DRIVEREG[DRIVE_PCL_ADL]) cpu->ADL[i] = cpu->PCL[i];
+        if (cpu->DRIVEREG[DRIVE_PCL_DB]) cpu->DB[i] = cpu->PCL[i];
+
+        carry_in = carry_out;
+        if ( i & 1 ) {
+            carry_out = NAND( cpu->PCLS[i], carry_in );
+            if (cpu->PHI2)
+                cpu->PCL[i] = NOR(cpu->PCLS[i], carry_in) | NOT(carry_out);
+        }
+        else {
+            carry_out = NOR( NOT(cpu->PCLS[i]), carry_in );
+            if (cpu->PHI2)
+                cpu->PCL[i] = NOR( NOT(cpu->PCLS[i]) & carry_in, carry_out );
+        }
+
+        if (cpu->DRIVEREG[DRIVE_PCL_PCL]) cpu->PCLS[i] = cpu->PCL[i];
+        if (cpu->PCLS[i] == 0) PCLC = 0;
+    }
+
+    printf ( "PCLC = %i\n", PCLC );
+
+    // HIGH
+
+    for (i=0; i<8; i++) {
+        if (cpu->DRIVEREG[DRIVE_ADH_PCH]) cpu->PCHS[i] = cpu->ADH[i];
+        if (cpu->DRIVEREG[DRIVE_PCH_ADH]) cpu->ADH[i] = cpu->PCH[i];
+        if (cpu->DRIVEREG[DRIVE_PCH_DB]) cpu->DB[i] = cpu->PCH[i];
+        if (cpu->DRIVEREG[DRIVE_PCH_PCH]) cpu->PCHS[i] = cpu->PCH[i];
+    }
 }
