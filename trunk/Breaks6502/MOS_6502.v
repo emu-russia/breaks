3bd0877450a1b6a20a85088d0df935711eaa5bd0
ogamespec@gmail.com <ogamespec@gmail.com@92c399df-91ff-7c34-b1d0-cc9d408d2d33>
2014-11-23 17:37:54 +0000

    Verilog : Dispatcher

--- trunk/Breaks6502/MOS_6502.v
+++ trunk/Breaks6502/MOS_6502.v
@@ -547,28 +547,155 @@ endmodule   // BranchLogic
 
 module Dispatcher (
     // Outputs
-
+    _ready, _IPC, _T0X, _T1X, T0, T1, _T2, _T3, _T4, _T5, T5, T6, RD, Z_IR, FETCH, RW, SYNC, ACRL2, 
     // Inputs
-
+    PHI0, RDY,
+    DORES, RESP, B_OUT, BRK6E, BRFW, _BRTAKEN, ACR, _ADL_PCL, PC_DB, BR2, BR3, _IMPLIED, _TWOCYCLE, 
+    decoder
 );
 
+    input PHI0, RDY;
+    input DORES, RESP, B_OUT, BRK6E, BRFW, _BRTAKEN, ACR, _ADL_PCL, PC_DB, BR2, BR3, _IMPLIED, _TWOCYCLE;
+    input [129:0] decoder;
+
+    output _ready, _IPC, _T0X, _T1X, T0, T1, _T2, _T3, _T4, _T5, T5, T6, RD, Z_IR, FETCH, RW, SYNC, ACRL2;
+    wire _ready, _IPC, _T0X, _T1X, T0, T1, _T2, _T3, _T4, _T5, T5, T6, RD, Z_IR, FETCH, RW, SYNC;
+
+    // Clocks
+    wire PHI1, PHI2;
+    assign PHI1 = ~PHI0;
+    assign PHI2 = PHI0;
+
+    // Misc
+    wire _MemOP, STOR, _SHIFT, _STORE;
+    assign _MemOP = ~( decoder[111] | decoder[122] | decoder[123] | decoder[124] | decoder[125] );
+    assign STOR = ~( ~decoder[97] | _MemOP );
+    assign _SHIFT = ~( decoder[106] | decoder[107] );
+    assign _STORE = ~decoder[97];
+
     // Ready Control
+    wire Ready1_Out, Ready2_Out;
+    mylatch Ready1 ( Ready1_Out, ~(RDY | Ready2_Out), PHI2 );
+    mylatch Ready2 ( Ready2_Out, WR, PHI1 );
+    assign _ready = Ready1_Out;
 
     // R/W Control
+    wire WRLatch_Out, RWLatch_Out;
+    wire WR;
+    mylatch WRLatch ( WRLatch_Out, ~( decoder[98] | decoder[100] | T5 | STOR | T6 | PC_DB), PHI2 );
+    assign WR = ~ ( _ready | REST | WRLatch_Out );
+    mylatch RWLatch ( RWLatch_Out, WR, PHI1 );
+    assign RW = ~RWLatch_Out;
+    assign RD = (PHI1 | ~RWLatch_Out);
 
     // Short Cycle Counter (T0-T1)
+    wire TRESXLatch_Out, TWOCYCLELatch_Out, TRES1Latch_Out, T0Latch_Out, T1Latch_Out;
+    mylatch TRESXLatch ( TRESXLatch_Out, TRESX, PHI1 );
+    mylatch TWOCYCLELatch ( TWOCYCLELatch_Out, _TWOCYCLE, PHI1 );
+    mylatch TRES1Latch ( TRES1Latch_Out, TRES1, PHI1 );
+    assign _T0X = ~( (~(TRESXLatch_Out&TWOCYCLELatch_Out) & ~TRES1Latch_Out) | ~(T0Latch_Out | T1Latch_Out) );
+    assign T0 = ~_T0X;
+    mylatch T0Latch ( T0Latch_Out, _T0X, PHI2 );
+    mylatch T1Latch ( T1Latch_Out, ~(T0Latch_Out | _ready), PHI1 );
+    assign _T1X = ~T1Latch_Out;
 
-    // Long Cycle Counter (T2-T5)
+    // Long Cycle Counter (T2-T5) (Shift Register)
+    wire T1InputLatch_Out;
+    mylatch T1InputLatch ( T1InputLatch_Out, T1, PHI2 );
+
+    wire LatchIn_T2_Out, LatchOut_T2_Out;
+    mylatch LatchIn_T2 ( LatchIn_T2_Out, _ready ? LatchOut_T2_Out : ~T1InputLatch_Out, PHI1 );
+    mylatch LatchOut_T2 ( LatchOut_T2_Out, ~(LatchIn_T2_Out | TRES2), PHI2 );
+    assign _T2 = (LatchIn_T2_Out | TRES2 );
+
+    wire LatchIn_T3_Out, LatchOut_T3_Out;
+    mylatch LatchIn_T3 ( LatchIn_T3_Out, _ready ? LatchOut_T3_Out : ~LatchOut_T2_Out, PHI1 );
+    mylatch LatchOut_T3 ( LatchOut_T3_Out, ~(LatchIn_T3_Out | TRES2), PHI2 );
+    assign _T3 = (LatchIn_T3_Out | TRES2 );
+
+    wire LatchIn_T4_Out, LatchOut_T4_Out;
+    mylatch LatchIn_T4 ( LatchIn_T4_Out, _ready ? LatchOut_T4_Out : ~LatchOut_T3_Out, PHI1 );
+    mylatch LatchOut_T4 ( LatchOut_T4_Out, ~(LatchIn_T4_Out | TRES2), PHI2 );
+    assign _T4 = (LatchIn_T4_Out | TRES2 );
+
+    wire LatchIn_T5_Out, LatchOut_T5_Out;
+    mylatch LatchIn_T5 ( LatchIn_T5_Out, _ready ? LatchOut_T5_Out : ~LatchOut_T4_Out, PHI1 );
+    mylatch LatchOut_T5 ( LatchOut_T5_Out, ~(LatchIn_T5_Out | TRES2), PHI2 );
+    assign _T5 = (LatchIn_T5_Out | TRES2 );
 
     // Extra Cycle Counter (T5-T6)
+    wire T56Latch_Out, T5Latch1_Out, T2Latch2_Out, T6Latch1_Out, T6Latch2_Out;
+    mylatch T56Latch ( T56Latch_Out, ~(_SHIFT | _MemOP | _ready), PHI2 );
+    mylatch T5Latch1 ( T5Latch1_Out, ~(T5Latch2_Out & _ready) & ~T56Latch_Out, PHI1 );
+    mylatch T5Latch2 ( T5Latch2_Out, ~T5Latch1_Out, PHI2 );
+    mylatch T6Latch1 ( T6Latch1_Out, ~(~T5Latch1_Out & ~_ready), PHI2 );
+    mylatch T6Latch2 ( T6Latch2_Out, ~T5Latch1_Out, PHI1 );
+    assign T5 = ~T5Latch1_Out;
+    assign T6 = T6Latch2_Out;
 
     // Instruction Termination (reset cycle counters)
+    wire REST, ENDS, ENDX, TRES2;
+    wire ENDS1_Out, ENDS2_Out;
+    assign REST = ~(_STORE & _SHIFT) & DORES;
+
+    mylatch ENDS1 ( ENDS1_Out, _ready ? ~T1 : (~(_BRTAKEN & BR2) & ~T0), PHI2 );
+    mylatch ENDS2 ( ENDS2_Out, RESP, PHI2 );
+    assign ENDS = ~( ENDS1_Out | ENDS2_Out );
+
+    wire temp;
+    assign temp = ~( decoder[100] | decoder[101] | decoder[102] | decoder[103] | decoder[104] | decoder[105]);
+    assign ENDX = ~( ~temp | T6 | BR3 | ~(_MemOP | decoder[96] | ~_SHIFT) );
+
+    wire ReadyPhi1_Out, RESP1_Out, RESP2_Out, T1L_Out;
+    mylatch ReadyPhi1 ( ReadyPhi1_Out, ~_ready, PHI1 );
+    mylatch RESP1 ( RESP1_Out, ~(RESP | ReadyPhi1_Out | RESP2_Out), PHI2 );
+    mylatch RESP2 ( RESP2_Out, ~(RESP1_Out | Brfw), PHI1 );
+    mylatch T1L ( T1L_Out, ~TRES1, PHI1 );
+    assign T1 = ~T1L_Out;
+    assign TRES1 = (ENDS | ~(_ready | ~(RESP1_Out | Brfw) ) );
+    assign SYNC = T1;
+
+    wire TRESX1_Out, TRESX2_Out;
+    mylatch TRESX1 ( TRESX1_Out, ~(decoder[91] | decoder[92]), PHI2);
+    mylatch TRESX2 ( TRESX2_Out, ~( RESP | ENDS | ~(_ready | ENDX) ), PHI2 );
+    assign TRESX = ~(BRK6E | ~(_ready | ACRL1 | REST | TRESX1_Out) | ~TRESX2_Out);
+
+    wire TRES2Latch_Out;
+    mylatch TRES2Latch ( TRES2Latch_Out, TRESX, PHI1 );
+    assign TRES2 = ~TRES2Latch_Out;
 
     // ACR Latch
+    wire ACRL1, ACRL2;
+    wire ACRL1Latch_Out, ACRL2Latch_Out;
+    assign ACRL2 = ~(~ACR & ~ReadyDelay) & (~ReadyDelay | ~ACRL1Latch_Out);
+    mylatch ACRL1Latch ( ACRL1Latch_Out, ~ACRL2Latch_Out, PHI2 );
+    mylatch ACRL2Latch ( ACRL2Latch_Out, ACRL2, PHI1 );
+    assign ACRL1 = ~ACRL1Latch_Out;
 
     // Program Counter Increment Control
+    wire ReadyDelay, Brfw;
+
+    wire DelayLatch1_Out, DelayLatch2_Out;
+    mylatch DelayLatch1 ( DelayLatch1_Out, _ready, PHI1 );
+    mylatch DelayLatch2 ( DelayLatch2_Out, ~DelayLatch1_Out, PHI2 );
+    assign ReadyDelay = ~DelayLatch2_Out;
+
+    wire BRFWLatch_Out;
+    mylatch BRFWLatch ( BRFWLatch_Out, ~(~BR3 | ReadyDelay), PHI2 );
+    assign Brfw = ~(BRFW ^ ACR) & BRFWLatch_Out;
+
+    wire RouteCLatch_Out, a_out, b_out, c_out;
+    mylatch RouteCLatch ( RouteCLatch_Out, ~(BR2 & _BRTAKEN) & (_ADL_PCL | BR2 | BR3), PHI2 );
+    mylatch c_latch ( c_out, ~(RouteCLatch_Out | _ready | ~_IMPLIED), PHI1 );
+    mylatch a_latch ( a_out, B_OUT, PHI1 );
+    mylatch b_latch ( b_out, Brfw, PHI1 );
+    assign _IPC = ~(a_out & (b_out | c_out));
 
     // Fetch Control
+    wire FetchLatch_Out;
+    mylatch FetchLatch ( FetchLatch_Out, T1, PHI2 );
+    assign FETCH = ~( _ready | ~FetchLatch_Out );
+    assign Z_IR = ~( B_OUT & FETCH );
 
 endmodule   // Dispatcher
 
@@ -1068,7 +1195,7 @@ module Core6502 (
 
     input   PHI0, _NMI, _IRQ, _RES, RDY, SO;
 
-    output  PHI1, PHI2, RW, SYNC;  
+    output  PHI1, PHI2, RW, SYNC;
     output [15:0]  ADDR;
 
     inout [7:0]   DATA;
@@ -1081,29 +1208,26 @@ module Core6502 (
 
     wire [7:0]   DATA;
     wire [15:0]  ADDR;
+    
+    // Clock Generator.
+    assign PHI1 = ~PHI0;
+    assign PHI2 = PHI0;
 
     // Internal wires
     
-    wire ACR, AVR;
-
-    // Internal buses
-    wire [7:0]  DB, SB, ADH, ADL;
+    wire DORES, RESP, BRK6E, B_OUT;
     
+    wire _ready, T0, T1, T5, T6, BR2, BR3, BRK5, _ADL_PCL, PC_DB, ACRL2;
+
+    wire BRFW, _BRTAKEN;
+
+    wire _C_OUT, _V_OUT, _N_OUT, _Z_OUT, _D_OUT, _I_OUT;
+
+    wire ACR, AVR;
     wire Z_ADD, SB_ADD, DB_ADD, NDB_ADD, ADL_ADD, SB_AC;
     wire ORS, ANDS, EORS, SUMS, SRS;
     wire ADD_SB06, ADD_SB7, ADD_ADL, AC_SB, AC_DB;
     wire _ADDC, _DAA, _DSA;
-
-    // Clock Generator.
-    assign PHI1 = ~PHI0;
-    assign PHI2 = PHI0;
-    
-    wire DORES, RESP, BRK6E, B_OUT;
-    wire _I_OUT, BR2, T0, BRK5, _ready;
-
-    wire BRFW, _BRTAKEN;
-
-    wire _C_OUT, _V_OUT, _N_OUT, _Z_OUT, _D_OUT;
 
     wire P_DB, DB_P, DBZ_Z, DB_N, IR5_C, ACR_C, DB_C, IR5_D, IR5_I, AVR_V, DB_V, ZERO_V, ONE_V;
 
@@ -1120,6 +1244,9 @@ module Core6502 (
 
     wire FETCH, Z_IR, _IMPLIED, _TWOCYCLE;
 
+    // Internal buses
+    wire [7:0]  DB, SB, ADH, ADL;
+
     // Break that shit.
 
     InterruptControl interrupts (
@@ -1135,7 +1262,13 @@ module Core6502 (
         P_DB, DB_P, DBZ_Z, DB_N, IR5_C, ACR_C, DB_C, IR5_D, IR5_I, AVR_V, DB_V, ZERO_V, ONE_V, 
         ~IR[5], ACR, AVR, B_OUT,
         DB
-    );   
+    );
+
+    Dispatcher dispatch (
+        _ready, _IPC, _T[0], _T[1], T0, T1, _T[2], _T[3], _T[4], _T[5], T5, T6, RD, Z_IR, FETCH, RW, SYNC, ACRL2, 
+        PHI0, RDY,
+        DORES, RESP, B_OUT, BRK6E, BRFW, _BRTAKEN, ACR, _ADL_PCL, PC_DB, BR2, BR3, _IMPLIED, _TWOCYCLE, 
+        decoder );
 
     ALU alu ( ACR, AVR, PHI0, 
         Z_ADD, SB_ADD, DB_ADD, NDB_ADD, ADL_ADD, SB_AC,
@@ -1178,7 +1311,6 @@ module Core6502 (
         DB, ADL, ADH );
 
 endmodule   // Core6502
-
 
 // --------------------------------------------------------------------------------
 // Test application / Icarus Verilog.
