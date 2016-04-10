80061877e0aa2bc5040da0436a8abbb60e7b673b
Andruxa <ogamespec@gmail.com>
2016-04-10 17:53:10 +0300

    Added smpte
    Smpte bars dummy

---
+++ Breaknes/Smpte.cs
@@ -0,0 +1,27 @@
+// SMPTE Bars generator
+
+// This module generates complete frame of smpte bars in form of NTSC signal.
+// Later signal sample data can be used to test TV module output.
+//
+
+public class Smpte
+{
+    public float[] SampleData;
+    public const int Stride = 12;
+    public const int LinesPerFrame = 480;
+
+    public Smpte()
+    {
+
+    }
+
+    public void GeneratePattern (bool OddLinesFirst)
+    {
+
+    }
+
+    private void Line (int num)
+    {
+
+    }
+}
