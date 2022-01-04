e345a946f484426c24ff0daaec976c05acbf756c
ogamespec <ogamespec@gmail.com>
2022-01-04 18:38:15 +0300

    perf timer

--- Breaknes/Breaknes.cpp
+++ Breaknes/Breaknes.cpp
@@ -1,17 +1,35 @@
 #include "pch.h"
 
+#ifdef _WINDOWS
+static int GetTimeStamp()
+{
+	FILETIME ft;
+	GetSystemTimeAsFileTime(&ft);
+	unsigned long long tt = ft.dwHighDateTime;
+	tt <<= 32;
+	tt |= ft.dwLowDateTime;
+	tt /= 10;
+	tt -= 11644473600000000ULL;
+	return (int)tt;
+}
+#endif
+
 int main()
 {
     Breaknes::Famicom* fami = new Breaknes::Famicom;
 
-    size_t maxHalfcycles = 10000;         // DEBUG
+    size_t maxHalfcycles = 1000000;         // DEBUG
+
+	int ts1 = GetTimeStamp();
 
     for (size_t n = 0; n < maxHalfcycles; n++)
     {
         fami->sim();
     }
 
+	int ts2 = GetTimeStamp();
+
     delete fami;
 
-    printf("Done!\n");
+    printf("Done in %d microseconds!\n", ts2 - ts1);
 }
--- Breaknes/pch.h
+++ Breaknes/pch.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <Windows.h>
 
 #include "BaseLogic.h"
 
