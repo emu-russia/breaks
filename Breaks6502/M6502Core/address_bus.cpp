b185f0459fa3c46b24a64554e33048aa6e814f14
ogamespec <ogamespec@gmail.com>
2022-01-07 16:42:58 +0300

    Update address_bus.cpp

--- Breaks6502/M6502Core/address_bus.cpp
+++ Breaks6502/M6502Core/address_bus.cpp
@@ -46,25 +46,10 @@ namespace M6502Core
 			{
 				ABH[n].set(NOT(NOT(ADH[n])));
 			}
+
+			cpu_out[(size_t)OutputPad::A0 + n] = ABL[n].get();
+			cpu_out[(size_t)OutputPad::A8 + n] = ABH[n].get();
 		}
-
-		cpu_out[(size_t)OutputPad::A0] = ABL[0].get();
-		cpu_out[(size_t)OutputPad::A1] = ABL[1].get();
-		cpu_out[(size_t)OutputPad::A2] = ABL[2].get();
-		cpu_out[(size_t)OutputPad::A3] = ABL[3].get();
-		cpu_out[(size_t)OutputPad::A4] = ABL[4].get();
-		cpu_out[(size_t)OutputPad::A5] = ABL[5].get();
-		cpu_out[(size_t)OutputPad::A6] = ABL[6].get();
-		cpu_out[(size_t)OutputPad::A7] = ABL[7].get();
-
-		cpu_out[(size_t)OutputPad::A8] = ABH[0].get();
-		cpu_out[(size_t)OutputPad::A9] = ABH[1].get();
-		cpu_out[(size_t)OutputPad::A10] = ABH[2].get();
-		cpu_out[(size_t)OutputPad::A11] = ABH[3].get();
-		cpu_out[(size_t)OutputPad::A12] = ABH[4].get();
-		cpu_out[(size_t)OutputPad::A13] = ABH[5].get();
-		cpu_out[(size_t)OutputPad::A14] = ABH[6].get();
-		cpu_out[(size_t)OutputPad::A15] = ABH[7].get();
 	}
 
 	uint8_t AddressBus::getABL()
