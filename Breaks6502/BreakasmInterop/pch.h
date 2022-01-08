3a0dd6d9a3722cc50444c9be2c031ea8111dca22
ogamespec <ogamespec@gmail.com>
2022-01-08 17:25:04 +0300

    Interop DLLs

---
+++ Breaks6502/BreakasmInterop/pch.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <Windows.h>
+
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#include "../Breakasm/ASM.h"
+#include "../Breakasm/ASMOPS.h"
