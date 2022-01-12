c4ba2afc52fa167078bfb54df75706cf4ea48d48
ogamespec <ogamespec@gmail.com>
2022-01-12 22:42:34 +0300

    Update extra_counter.cpp

--- Breaks6502/M6502Core/extra_counter.cpp
+++ Breaks6502/M6502Core/extra_counter.cpp
@@ -15,20 +15,24 @@ namespace M6502Core
 		t1_latch.set(T1, PHI2);
 
 		t2_latch1.set(MUX(n_ready, t1_latch.nget(), t2_latch2.nget()), PHI1);
-		t2_latch2.set(NOR(t2_latch1.get(), TRES2), PHI2);
+		TriState T2 = NOR(t2_latch1.get(), TRES2);
+		t2_latch2.set(T2, PHI2);
 
 		t3_latch1.set(MUX(n_ready, t2_latch2.nget(), t3_latch2.nget()), PHI1);
-		t3_latch2.set(NOR(t3_latch1.get(), TRES2), PHI2);
+		TriState T3 = NOR(t3_latch1.get(), TRES2);
+		t3_latch2.set(T3, PHI2);
 
 		t4_latch1.set(MUX(n_ready, t3_latch2.nget(), t4_latch2.nget()), PHI1);
-		t4_latch2.set(NOR(t4_latch1.get(), TRES2), PHI2);
+		TriState T4 = NOR(t4_latch1.get(), TRES2);
+		t4_latch2.set(T4, PHI2);
 
 		t5_latch1.set(MUX(n_ready, t4_latch2.nget(), t5_latch2.nget()), PHI1);
-		t5_latch2.set(NOR(t5_latch1.get(), TRES2), PHI2);
+		TriState T5 = NOR(t5_latch1.get(), TRES2);
+		t5_latch2.set(T5, PHI2);
 
-		outputs[(size_t)ExtraCounter_Output::n_T2] = NOT(NOR(t2_latch1.get(), TRES2));
-		outputs[(size_t)ExtraCounter_Output::n_T3] = NOT(NOR(t3_latch1.get(), TRES2));
-		outputs[(size_t)ExtraCounter_Output::n_T4] = NOT(NOR(t4_latch1.get(), TRES2));
-		outputs[(size_t)ExtraCounter_Output::n_T5] = NOT(NOR(t5_latch1.get(), TRES2));
+		outputs[(size_t)ExtraCounter_Output::n_T2] = NOT(T2);
+		outputs[(size_t)ExtraCounter_Output::n_T3] = NOT(T3);
+		outputs[(size_t)ExtraCounter_Output::n_T4] = NOT(T4);
+		outputs[(size_t)ExtraCounter_Output::n_T5] = NOT(T5);
 	}
 }
