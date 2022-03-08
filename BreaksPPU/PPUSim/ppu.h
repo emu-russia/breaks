3bbac539449f456ffd846bd307bfaa274951b516
ogamespec <ogamespec@gmail.com>
2022-03-08 17:22:48 +0300

    PPUSim WIP

--- BreaksPPU/PPUSim/ppu.h
+++ BreaksPPU/PPUSim/ppu.h
@@ -1,5 +1,21 @@
 #pragma once
 
+#include "bgcol.h"
+#include "cram.h"
+#include "dataread.h"
+#include "fifo.h"
+#include "fsm.h"
+#include "hv.h"
+#include "hv_decoder.h"
+#include "mux.h"
+#include "oam.h"
+#include "pargen.h"
+#include "regs.h"
+#include "scroll_regs.h"
+#include "sprite_eval.h"
+#include "video_out.h"
+#include "vram_ctrl.h"
+
 namespace PPUSim
 {
 
