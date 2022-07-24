0faec2fdaa16fdc9940690b519bc4005983cb92d
ogamespec <ogamespec@gmail.com>
2022-07-24 17:58:01 +0300

    Common\dlatch.v for ICARUS

--- HDL/Framework/Icarus/run.bat
+++ HDL/Framework/Icarus/run.bat
@@ -1,2 +1,2 @@
-iverilog -o apu_run ../../Common/*.v ../../APU/*.v ../../Core6502/*.v ../../PPU/*.v apu_run.v
+iverilog -D ICARUS -o apu_run ../../Common/*.v ../../APU/*.v ../../Core6502/*.v ../../PPU/*.v apu_run.v
 vvp apu_run
