2fe7fb8c76eaded83729a238ed397e57b3df04c5
ogamespec <ogamespec@gmail.com>
2022-12-20 10:40:14 +0300

    clkgen.v cleanup

--- HDL/APU/core.v
+++ HDL/APU/core.v
@@ -34,3 +34,77 @@ module Core(
 		.D(core_DPads) );
 
 endmodule // Core
+
+module CLK_Divider(n_CLK_frompad, PHI0_tocore, PHI2_fromcore, n_M2_topad);
+
+	input n_CLK_frompad;
+	output PHI0_tocore;
+	input PHI2_fromcore;
+	output n_M2_topad;
+
+	wire [5:0] sout;
+	wire rst;
+	wire q;
+	wire nq;
+	wire nval_4;
+
+	DivPhaseSpiltter phase_split (
+		.n_clk(n_CLK_frompad),
+		.q(q),
+		.nq(nq) );
+
+	assign PHI0_tocore = ~sout[5];
+	nor (rst, PHI0_tocore, sout[4]);
+
+	DivSRBit sr0 (.q(q), .nq(nq), .rst(rst), .sin(PHI0_tocore), .sout(sout[0]));
+	DivSRBit sr1 (.q(q), .nq(nq), .rst(rst), .sin(sout[0]), .sout(sout[1]));
+	DivSRBit sr2 (.q(q), .nq(nq), .rst(rst), .sin(sout[1]), .sout(sout[2]));
+	DivSRBit sr3 (.q(q), .nq(nq), .rst(rst), .sin(sout[2]), .sout(sout[3]));
+	DivSRBit sr4 (.q(q), .nq(nq), .rst(1'b0), .sin(sout[3]), .sout(sout[4]), .n_val(nval_4));
+	DivSRBit sr5 (.q(q), .nq(nq), .rst(1'b0), .sin(sout[4]), .sout(sout[5]));
+
+	nor (n_M2_topad, nval_4, PHI2_fromcore);
+
+endmodule // CLK_Divider
+
+module DivSRBit(q, nq, rst, sin, n_val, sout);
+
+	input q;
+	input nq;
+	input rst;
+	input sin;
+	output n_val;
+	output sout;
+
+	wire out_val;
+
+	dlatch in_latch (
+		.d(sin),
+		.en(q),
+		.nq(n_val) );
+
+	dlatch out_latch (
+		.d(n_val),
+		.en(nq),
+		.q(out_val) );
+
+	nor (sout, out_val, rst);
+
+endmodule // DivSRBit
+
+module DivPhaseSpiltter(n_clk, q, nq);
+
+	input n_clk;
+	output q;
+	output nq;
+
+	(* keep = "true" *) wire not1_out;
+	(* keep = "true" *) wire not2_out;
+
+	assign not1_out = ~n_clk;
+	assign not2_out = ~not1_out;
+
+	nor (nq, not1_out, q);
+	nor (q, nq, not2_out);
+
+endmodule // DivPhaseSpiltter
