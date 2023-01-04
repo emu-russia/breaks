33ab43fb9f37d25d1692f0ff4d79d52b33a205e0
ogamespec <ogamespec@gmail.com>
2023-01-05 02:31:42 +0300

    auxb

--- Scripts/APU_DAC_Generator.py
+++ Scripts/APU_DAC_Generator.py
@@ -11,12 +11,10 @@ import os
 import sys
 import csv
 
-IntRes = 5.1 * 1000 	# Internal single MOSFET resistance
+IntRes = 39 * 1000 		# Internal single MOSFET resistance
 IntUnloaded = 999999 	# Non-loaded DAC internal resistance 
 ExtRes = 100 			# On-board pull-down resistor to GND
 Vdd = 5.0
-
-# According to the results of comparisons with real measurements, the internal resistance of the FET is out of the nominal resistance range (E24 5.1k)
 
 """
 	Recall the school curriculum.
@@ -39,7 +37,7 @@ def SchoolTest():
 def AUX_A_Resistance (sqa, sqb):
 	sqa_r_exists = [0, 0, 0, 0]
 	sqb_r_exists = [0, 0, 0, 0]
-	stage_reciprocal = [8, 3, 2, 1]
+	stage_reciprocal = [1, 2, 4, 8]
 
 	# First determine which resistances are present
 	for bit in range(4):
@@ -53,10 +51,10 @@ def AUX_A_Resistance (sqa, sqb):
 	r = 0
 	for i in range(4):
 		if sqa_r_exists[i]:
-			r = r + 1 / IntRes / stage_reciprocal[i]
+			r = r + 1 / (IntRes / stage_reciprocal[i])
 			any_exists = True
 		if sqb_r_exists[i]:
-			r = r + 1 / IntRes / stage_reciprocal[i]
+			r = r + 1 / (IntRes / stage_reciprocal[i])
 			any_exists = True
 
 	if any_exists == False:
@@ -64,13 +62,46 @@ def AUX_A_Resistance (sqa, sqb):
 
 	return 1 / r
 
-def DumpPrint():
-	for sqb in range(16):
-		for sqa in range(16):
-			r = AUX_A_Resistance (sqa, sqb)
-			i = Vdd / (r + ExtRes)
-			aux_v = i * ExtRes
-			print (f"sqb:{sqb} sqa:{sqa}: {aux_v * 1000} mV")
+"""
+	Return the internal resistance of the AUX B terminal based on the TRI/RND/DMC digital inputs.
+"""
+def AUX_B_Resistance (tri, rnd, dmc):
+	tri_r_exists = [0, 0, 0, 0]
+	rnd_r_exists = [0, 0, 0, 0]
+	dmc_r_exists = [0, 0, 0, 0, 0, 0, 0]
+	tri_reciprocal = [2, 4, 8, 16]
+	rnd_reciprocal = [1, 2, 4, 8]
+	dmc_reciprocal = [0.5, 1, 2, 4, 8, 16, 32]
+
+	# First determine which resistances are present
+	for bit in range(4):
+		if (tri >> bit) & 1:
+			tri_r_exists[bit] = 1
+		if (rnd >> bit) & 1:
+			rnd_r_exists[bit] = 1
+	for bit in range(7):
+		if (dmc >> bit) & 1:
+			dmc_r_exists[bit] = 1
+
+	# Calculate the total resistance
+	any_exists = False
+	r = 0
+	for i in range(4):
+		if tri_r_exists[i]:
+			r = r + 1 / (IntRes / tri_reciprocal[i])
+			any_exists = True
+		if rnd_r_exists[i]:
+			r = r + 1 / (IntRes / rnd_reciprocal[i])
+			any_exists = True
+	for i in range(7):
+		if dmc_r_exists[i]:
+			r = r + 1 / (IntRes / dmc_reciprocal[i])
+			any_exists = True
+
+	if any_exists == False:
+		return IntUnloaded
+
+	return 1 / r
 
 def DumpCsv():
 	with open('auxa.csv', 'w', encoding='UTF8', newline='') as f:
@@ -84,9 +115,19 @@ def DumpCsv():
 				aux_v = i * ExtRes
 				row = [(sqb << 4) | sqa, aux_v * 1000]
 				writer.writerow(row)
-	return
+	with open('auxb.csv', 'w', encoding='UTF8', newline='') as f:
+		writer = csv.writer(f)
+		header = ['dmc.rnd.tri', 'mV']
+		writer.writerow(header)
+		for dmc in range(128):
+			for rnd in range(16):
+				for tri in range(16):
+					r = AUX_B_Resistance (tri, rnd, dmc)
+					i = Vdd / (r + ExtRes)
+					aux_v = i * ExtRes
+					row = [(dmc << 8) | (rnd << 4) | tri, aux_v * 1000]
+					writer.writerow(row)
 
 if __name__ == '__main__':
 	#SchoolTest()
-	#DumpPrint()
 	DumpCsv()
