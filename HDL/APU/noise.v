c76d04d6be6e785e1a25cd0e27addc512bd48b4b
ogamespec <ogamespec@gmail.com>
2023-01-14 20:39:02 +0300

    Updated design

--- HDL/APU/noise.v
+++ HDL/APU/noise.v
@@ -1,7 +1,7 @@
 
 module NoiseChan(
 	n_ACLK, ACLK, 
-	RES, DB, W400A, W400B, W400C, W400E, W400F, nLFO1, RND_LC, NORND, LOCK, 
+	RES, DB, W400C, W400E, W400F, nLFO1, RND_LC, NORND, LOCK, 
 	RND_out);
 
 	input n_ACLK;
@@ -9,8 +9,6 @@ module NoiseChan(
 
 	input RES;
 	inout [7:0] DB;
-	input W400A;
-	input W400B;
 	input W400C;
 	input W400E;
 	input W400F;
