80e3ee9a0d799a68ca363387031e0a795d18ae53
ogamespec <ogamespec@gmail.com>
2023-01-16 01:26:49 +0300

    square_output.v wip

--- HDL/Framework/Icarus/apu/square_output.bat
+++ HDL/Framework/Icarus/apu/square_output.bat
@@ -1,2 +1,2 @@
-iverilog -D ICARUS -o square_output.run ../../../Common/*.v ../../../APU/*.v ../../../Core6502/*.v square_output.v aclkgen.v
+iverilog -D ICARUS -o square_output.run ../../../Common/*.v ../../../APU/*.v ../../../Core6502/*.v square_output.v
 vvp square_output.run
