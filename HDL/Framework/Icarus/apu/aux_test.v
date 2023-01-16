bf983ff702cde7cf07d2e2a9c621b8967c34cec2
ogamespec <ogamespec@gmail.com>
2023-01-17 01:36:56 +0300

    AUX_Dumper

--- HDL/Framework/Icarus/apu/aux_test.v
+++ HDL/Framework/Icarus/apu/aux_test.v
@@ -2,16 +2,24 @@
 
 module AUX_Run ();
 
+	integer i;
+	integer fd;
+
 	reg [7:0] a;
 	reg [14:0] b;
+	reg [23:0] addr;
 
 	always #1 a = a + 1;
 	always #1 b = b + 1;
+	always #1 addr = addr + 1;
 
 	wire [31:0] aout;
 	wire [31:0] bout;
 
 	AUX aux (.AUX_A(a), .AUX_B(b), .AOut(aout), .BOut(bout) );
+
+	AUX_Dumper dump_a (.addr(addr), .val(aout));
+	AUX_Dumper dump_b (.addr(addr), .val(bout));
 
 	initial begin
 
@@ -23,8 +31,24 @@ module AUX_Run ();
 
 		a <= 0;
 		b <= 0;
+		addr <= 0;
 
 		repeat (32769) @ (b);
+
+		// Save to file
+
+		fd = $fopen("auxa_dump.bin", "wb");
+		for (i = 0; i<16777216; i=i+1) begin
+			$fwrite (fd, "%u", dump_a.mem[i]);
+		end
+		$fclose (fd);
+
+		fd = $fopen("auxb_dump.bin", "wb");
+		for (i = 0; i<16777216; i=i+1) begin
+			$fwrite (fd, "%u", dump_b.mem[i]);
+		end
+		$fclose (fd);
+
 		$finish;
 	end
 
--- HDL/Framework/Icarus/apu/auxout.v
+++ HDL/Framework/Icarus/apu/auxout.v
@@ -19,3 +19,21 @@ module AUX (AUX_A, AUX_B, AOut, BOut);
 	assign BOut = auxb_rom[AUX_B];
 
 endmodule // AUX
+
+module AUX_Dumper (addr, val);
+
+	input [23:0] addr;
+	input [31:0] val;
+
+	reg [31:0] mem [0:16777215];
+	integer i;
+
+	initial begin
+		for (i = 0; i<16777216; i=i+1) begin
+			mem[i] = 32'h00000000;
+		end
+	end
+
+	always @(*) mem[addr] = val;
+
+endmodule
