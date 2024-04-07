3af910d9901cc7603e30340eb079086e0398e22e
org <ogamespec@gmail.com>
2024-04-07 20:04:28 +0300

    pc_control.v

--- HDL/Core6502/bus_control.v
+++ HDL/Core6502/bus_control.v
@@ -1,8 +1,8 @@
 
 module Bus_Control(
 	PHI1, PHI2,
-	n_SBXY, AND, STOR, Z_ADL0, BR2, ACRL2, DL_PCH, n_ready, INC_SB, BRK6E, STXY, n_PCH_PCH,
-	T0, T1, T6, T7,
+	n_SBXY, AND, STOR, Z_ADL0, ACRL2, DL_PCH, n_ready, INC_SB, BRK6E, STXY, n_PCH_PCH,
+	T0, T1, T6, T7, BR0, 
 	X,
 	ZTST, PGX, 
 	Z_ADH0, Z_ADH17, SB_AC, ADL_ABL, AC_SB, SB_DB, AC_DB, SB_ADH, DL_ADH, DL_ADL, ADH_ABH, DL_DB);
@@ -14,7 +14,6 @@ module Bus_Control(
 	input AND;
 	input STOR;
 	input Z_ADL0;
-	input BR2;
 	input ACRL2;
 	input DL_PCH;
 	input n_ready;
@@ -27,6 +26,7 @@ module Bus_Control(
 	input T1;
 	input T6;
 	input T7;
+	input BR0;
 
 	input [129:0] X;
 
